8060fb2b1269835c1e568713600df1fdbfe92aae
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-01 16:11:12 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration[4.2]
   #define a change method in which to do the migration
   def change
-    create_table :actors do |t| 
+    create_table :actors do |t|
       t.string :first_name
       t.string :last_name
     end
